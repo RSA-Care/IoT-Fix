9802170eb963fd0f70b5b6f60210fe18488df0d0
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2024-06-24 21:36:35 +0700

    Change the oled height to 64

--- src/OLED/OLED.h
+++ src/OLED/OLED.h
@@ -5,7 +5,7 @@
 
 #define OLED_I2C_ADDRESS 0x3C
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1    // Reset pin
 
 void oledBegin();
